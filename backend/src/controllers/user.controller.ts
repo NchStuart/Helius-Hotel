a69aac0b3aa796501900b267b2a2dcdc0af5c813
Eduardo Soares Dutra <eduardosoaresdutra@gmail.com>
2023-04-14 17:14:22 -0300

    Adicionada criptografia para registro de usuários ao banco

--- backend/src/controllers/user.controller.ts
+++ backend/src/controllers/user.controller.ts
@@ -1,9 +1,15 @@
 import { Request, Response } from "express";
 import { createUser } from "../models/user.model";
 import { User } from "../util/types";
+import bcrypt from "bcrypt";
 
-function insertUser(req: Request, res: Response) {
-    const user: User = req.body
+async function insertUser(req: Request, res: Response) {
+    const user: User = req.body;
+    const {password} = user;
+
+    const hashPassword = await bcrypt.hash(password, 10);
+    user.password = hashPassword;
+
     createUser(user, function (error: Error) {
         error ? res.status(400).send("Erro ao registrar usuário") : null;
         res.status(200).send("Usuário criado!");
